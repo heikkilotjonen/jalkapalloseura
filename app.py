5d857fa3d891d593e9e5f6c03104cac238887903
Heikki Lötjönen <heikkimlotjonen@gmail.com>
2025-03-25 15:00:23 +0200

    Add application

--- app.py
+++ app.py
@@ -1,13 +1,41 @@
 import sqlite3
 from flask import Flask
-from flask import redirect, render_template, request
-from werkzeug.security import generate_password_hash
+from flask import redirect, render_template, request, session
+from werkzeug.security import generate_password_hash, check_password_hash
+import config
 
 app = Flask(__name__)
+app.secret_key = config.secret_key
 
 @app.route("/")
 def index():
-    return "Tähän tulee jalkapalloseura sovellus"
+    return render_template("index.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+
+    with sqlite3.connect("database.db") as db:
+        db.isolation_level = None
+        sql = "SELECT password_hash FROM users WHERE username = ?"
+        cursor = db.execute(sql, [username])
+        result = cursor.fetchone()
+
+    if result:
+        password_hash = result[0]
+        if check_password_hash(password_hash, password):
+            session["username"] = username
+            return redirect("/")
+        else:
+            return "VIRHE: väärä tunnus tai salasana"
+    else:
+        return "VIRHE: väärä tunnus tai salasana"
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
 
 @app.route("/register")
 def register():
